51ab1ae2de89eb684d9a0b63b1317b5fddf60141
Starlyyy <izabella.costa.tds2023@gmail.com>
2025-11-14 14:54:29 -0300

    ee

--- restaurante2.0.sql
+++ restaurante2.0.sql
@@ -1,11 +1,11 @@
 create table usuario(
     id_usuario int primary key AUTO_INCREMENT,
     nomeUsuario varchar(60) not null,
-    senhaUsuario int not null,
+    senhaUsuario varchar(20) not null,
     fotoUsuario varchar(200),
     isAdm char(1) not null,
 
-    constraint chk_usuario check (isAdm is ('S', 'N'))
+    constraint chk_usuario check (isAdm in ('S', 'N'))
 );
 
 insert into usuario (nomeUsuario, senhaUsuario, isAdm) values
